9e2e3afbd63ea2a70f90640225ed4f2955bf2f37
Jeremy Fleischman <jeremyfleischman@gmail.com>
2017-12-08 12:08:58 -0800

    Disable bluetooth on mobile and enable it when at desk.

--- bin/layouts/mission-desk.sh
+++ bin/layouts/mission-desk.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 xrandr --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output eDP1 --off --output DP2 --off
+fixtray
 
 # Switch to HDMI audio.
 pactl set-card-profile 1 output:hdmi-stereo+input:analog-stereo
 
-fixtray
+# Enable bluetooth
+rfkill unblock bluetooth
--- bin/layouts/mobile.sh
+++ bin/layouts/mobile.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 xrandr --output HDMI1 --off --output VIRTUAL1 --off --output DP1 --off --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP2 --off
+fixtray
 
 # Switch to laptop speakers.
 pactl set-card-profile 1 output:analog-stereo+input:analog-stereo
 
-fixtray
+# Disable bluetooth
+rfkill block bluetooth
