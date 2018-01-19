c657377c634abb8f113e615680aa70878a35842c
Jeremy Fleischman <jeremyfleischman@gmail.com>
2018-01-19 13:24:56 -0800

    Update autoperipherals in light of my fancy new usb c adapater at my desk.

--- bin/layouts/mission-desk.sh
+++ bin/layouts/mission-desk.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+# NOTE: This is basically a copy of honor-desk
+
 xrandr --output HDMI1 --auto --pos 0x0 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output eDP1 --off --output DP2 --off
 fixtray
 
