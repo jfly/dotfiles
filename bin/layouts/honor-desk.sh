0c58caf7dfb4008ff0053a4b1af741be78948cbc
Jeremy Fleischman <jeremyfleischman@gmail.com>
2018-01-23 10:54:21 -0800

    Hacky hack.

--- bin/layouts/honor-desk.sh
+++ bin/layouts/honor-desk.sh
@@ -6,6 +6,8 @@ xrandr --output DP1 --auto --pos 0x0 --rotate normal --output VIRTUAL1 --off --o
 fixtray
 
 # Switch to HDMI audio.
+# TODO - for some reason, only one of these profiles is available, but it seems to change randomly.
+pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:hdmi-stereo+input:analog-stereo
 pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:hdmi-stereo+input:stereo-fallback
 
 # Enable bluetooth
