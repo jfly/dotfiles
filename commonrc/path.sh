be834cc4e119e0be6d2c9d4ff751c5e4d90925c3
Jeremy Fleischman <jeremyfleischman@gmail.com>
2018-02-12 18:59:58 -0800

    Add pip local install directory to PATH

--- commonrc/path.sh
+++ commonrc/path.sh
@@ -2,3 +2,6 @@ export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/lib/jvm/default/bin:/u
 export PATH=$PATH:$HOME/bin
 export PATH=$PATH:$HOME/bin/layouts
 export PATH=$PATH:$HOME/.gem/ruby/2.5.0/bin
+
+# pip seems to install binaries to here
+export PATH=$PATH:$HOME/.local/bin/
