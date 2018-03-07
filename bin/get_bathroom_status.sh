77ac85d84ed925c92d9c8bf3a28383bc1cb27e0c
Jeremy Fleischman <jeremyfleischman@gmail.com>
2018-03-06 19:20:46 -0800

    Only run bathroom checker on dalinar.

--- bin/get_bathroom_status.sh
+++ bin/get_bathroom_status.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-OCCUPIED=$(curl -s http://g2g.honordev.com/api | jq ".occupied")
+if [ "$(hostname)" != "dalinar" ]; then
+    exit 0
+fi
+
+OCCUPIED=$(curl -s -m 0.1 http://g2g.honordev.com/api | jq ".occupied")
 
 if [ "$OCCUPIED" = "true" ]; then
-    echo "<fc=red>Not good to go</fc>"
+    echo -n "<fc=red>Not good to go</fc>"
+elif [ "$OCCUPIED" = "false" ]; then
+    echo -n "<fc=#006000>Good to go</fc>"
 else
-    echo "<fc=#006000>Good to go</fc>"
+    echo -n "Could not connect to g2g"
 fi
+echo " | "
