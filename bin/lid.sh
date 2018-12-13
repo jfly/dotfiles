b92f5e71f37ab916801e99249835813a80551fb1
Jeremy Fleischman <jeremyfleischman@gmail.com>
2018-12-12 23:30:32 -0800

    Adding some more trusted wifi networks.

--- bin/lid.sh
+++ bin/lid.sh
@@ -4,9 +4,8 @@ case "$1" in
     button/lid)
         case "$3" in
             close)
-                connection=$(nmcli -t connection show --active | cut -d : -f 1 | head -n 1)
-                if [ "$connection" == "dagron" ] || [ "$connection" == "Honor Ethernet" ] || [ "$connection" == "Honor" ] || [ "$connection" == "NSA Surveillance Van 37" ]; then
-                    logger "Ignoring lid close event because we're connected to: $connection"
+                if nmcli -t connection show --active | cut -d : -f 1 | grep "^\(Honor\|dagron\|Honor Ethernet\|NSA Surveillance Van 37\|Cal 3\)$" &> /dev/null; then
+                    logger "Ignoring lid close event because we're connected to: $(nmcli -t connection show --active | cut -d : -f 1)"
                 else
                     slock
                 fi
