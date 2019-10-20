d579908eab703caa619f03c70a33d00fcf60c6c5
Jeremy Fleischman <jeremyfleischman@gmail.com>
2019-10-20 10:22:25 -0700

    Remove create_ap.sh in favor of using the very awesome [create_ap] directly.
    [create_ap]: https://wiki.archlinux.org/index.php/software_access_point#create_ap

--- bin/create-ap.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "WIP WIP WIP this does not work yet"
-echo "This may be helpful: https://blog.thewalr.us/2017/09/26/raspberry-pi-zero-w-simultaneous-ap-and-managed-mode-wifi/"
-exit 1
-
-iw dev wlp4s0 interface add wlan0_sta type managed addr d6:56:a2:97:30:68
-iw dev wlp4s0 interface add wlan0_ap type managed addr d6:56:a2:97:30:69
