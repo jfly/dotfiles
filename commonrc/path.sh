31283fd432cbe4559e833c1aa56c1d02bac6f785
Jeremy Fleischman <jeremyfleischman@gmail.com>
2020-01-26 09:07:12 -0800

    Update to ruby 2.7

--- commonrc/path.sh
+++ commonrc/path.sh
@@ -1,6 +1,6 @@
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/lib/jvm/default/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl"
 export PATH=$PATH:$HOME/bin
-export PATH=$PATH:$HOME/.gem/ruby/2.6.0/bin
+export PATH=$PATH:$HOME/.gem/ruby/2.7.0/bin
 
 # `pip install --user` installs binaries here.
 export PATH=$PATH:$HOME/.local/bin
