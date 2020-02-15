3f12dc18511c03c39330d85ed60e6bca9ae6a11f
Jeremy Fleischman <jeremyfleischman@gmail.com>
2020-02-15 13:35:30 -0800

    Set up scanning

---
+++ bin/scan.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Built from https://wiki.archlinux.org/index.php/SANE#Verification
+
+# Got this device name from `scanimage -L`
+DEVICE=pixma:MG5400_192.168.1.7
+
+SCANS_DIR=$HOME/scans
+mkdir -p "$SCANS_DIR"
+FILENAME="$SCANS_DIR/$(date +"%Y-%m-%d_%H-%M-%S")_$HOSTNAME.png"
+
+scanimage --device "$DEVICE" --format=png --output-file "$FILENAME" --resolution 300 --progress
