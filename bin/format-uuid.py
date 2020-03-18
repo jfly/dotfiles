634097a6ea1a78b156b9d4b0cf7e81154e111d56
Jeremy Fleischman <jeremyfleischman@gmail.com>
2020-03-17 21:21:20 -0700

    Improved uuid formatter

---
+++ bin/format-uuid.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import ast
+import sys
+import uuid
+
+clip = sys.stdin.read().strip()
+if any(clip.startswith(prefix) for prefix in ['b"', '"', "'", "b'"]):
+    bs = ast.literal_eval(clip)
+    parsed = uuid.UUID(bytes=bs)
+else:
+    clip = clip.replace("0x", "")
+    parsed = uuid.UUID(hex=clip)
+
+pretty = str(parsed)
+formats = [
+    pretty,
+    pretty.replace("-", ""),
+    pretty.upper(),
+    pretty.upper().replace("-", ""),
+    "0x" + pretty.upper().replace("-", ""),
+]
+
+print("\n".join(formats))
