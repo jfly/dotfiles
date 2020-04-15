960af0ca2604161080f009db7227e8e2439cbe83
Jeremy Fleischman <jeremyfleischman@gmail.com>
2020-04-14 23:47:27 -0700

    Add support for scanning via usb.

--- bin/scan.sh
+++ bin/scan.sh
@@ -4,9 +4,11 @@
 
 # Got this device name from `scanimage -L`
 DEVICE=pixma:MG5400_192.168.1.7
+DEVICE=pixma:04A91764_00BA32
 
 SCANS_DIR=$HOME/scans
 mkdir -p "$SCANS_DIR"
 FILENAME="$SCANS_DIR/$(date +"%Y-%m-%d_%H-%M-%S")_$HOSTNAME.png"
 
 scanimage --device "$DEVICE" --format=png --output-file "$FILENAME" --resolution 300 --progress
+echo "Saved scan to $FILENAME"
