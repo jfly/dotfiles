7ae6690d05f4dfde8c8b0c563653266175bd382a
Jeremy Fleischman <jeremyfleischman@gmail.com>
2020-10-13 21:26:02 -0700

    Utils to turn mouse acceleration on/off

---
+++ bin/aoe-start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+xinput --set-prop 'M720 Triathlon Mouse' 'libinput Accel Profile Enabled' 0, 1
---
+++ bin/aoe-stop.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+xinput --set-prop 'M720 Triathlon Mouse' 'libinput Accel Profile Enabled' 1, 0
