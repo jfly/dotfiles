daf10b17631ff2a87053c56e37de86df392a6c58
Jeremy Fleischman <jeremyfleischman@gmail.com>
2020-12-08 22:09:27 -0800

    Fancier format-uuid

--- bin/format-uuid.py
+++ bin/format-uuid.py
@@ -4,13 +4,41 @@ import ast
 import sys
 import uuid
 
+
+def try_parse_bytes(s):
+    try:
+        bs = ast.literal_eval(clip)
+    except (SyntaxError, ValueError):
+        return None
+    try:
+        return uuid.UUID(bytes=bs)
+    except ValueError:
+        return None
+    except TypeError:
+        return None
+
+
+def try_parse_hex(s):
+    s = s.replace("0x", "")
+    try:
+        s = ast.literal_eval(s)
+    except (SyntaxError, ValueError):
+        pass
+
+    try:
+        return uuid.UUID(hex=s)
+    except ValueError:
+        return None
+
+
 clip = sys.stdin.read().strip()
-if any(clip.startswith(prefix) for prefix in ['b"', '"', "'", "b'"]):
-    bs = ast.literal_eval(clip)
-    parsed = uuid.UUID(bytes=bs)
-else:
-    clip = clip.replace("0x", "")
-    parsed = uuid.UUID(hex=clip)
+parsed = try_parse_bytes(clip)
+if parsed is None:
+    parsed = try_parse_hex(clip)
+
+if parsed is None:
+    print(f"This doesn't look like a hex or a bytes based uuid: {clip}")
+    sys.exit(1)
 
 pretty = str(parsed)
 formats = [
