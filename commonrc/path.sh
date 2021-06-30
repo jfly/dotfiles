1a19f67b9634f7fc97aa140e1050268f779d848d
Jeremy Fleischman <jeremyfleischman@gmail.com>
2021-06-30 09:55:26 -0700

    Stop using system ruby for development.
    asdf seems like a nicer setup.

--- commonrc/path.sh
+++ commonrc/path.sh
@@ -1,6 +1,5 @@
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/lib/jvm/default/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl"
 export PATH=$PATH:$HOME/bin
-export PATH=$PATH:$HOME/.gem/ruby/2.7.0/bin
 
 # `pip install --user` installs binaries here.
 export PATH=$PATH:$HOME/.local/bin
