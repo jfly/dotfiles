12ca360ec667b7fb8526e52828b16e4c19e888ab
Jeremy Fleischman <jeremyfleischman@gmail.com>
2021-10-17 22:42:24 -0700

    Oh nice, turns out you can store gtk themes in your home directory.
    Yay! Don't need to move this to aconfmgr.

--- aconfmgr-config/99-cleanup.sh
+++ aconfmgr-config/99-cleanup.sh
@@ -29,4 +29,3 @@ CreateLink /etc/systemd/system/slock@jeremy.service /home/jeremy/gitting/dotfile
 CreateLink /etc/udev/rules.d/10-local.rules /home/jeremy/gitting/dotfiles/etc/udev/rules.d/10-local.rules
 CreateLink /etc/udevmon.yaml /home/jeremy/gitting/dotfiles/etc/udevmon.yaml
 CreateLink /usr/lib/systemd/system/monitors.service /home/jeremy/gitting/dotfiles/units/monitors.service
-CreateLink /usr/share/themes/jfattymacs /home/jeremy/gitting/dotfiles/usr/share/themes/jfattymacs
