4ff3092c174c283790bb365551737789d4c9227d
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-01-24 00:08:39 -0800

    More nixification...

--- my-nix/default.nix
+++ my-nix/default.nix
@@ -1,6 +1,19 @@
 { pkgs ? (import ./sources.nix).nixos-master {} }:
 
+let symlink = target: name: pkgs.runCommand name {} ''
+    mkdir -p $out/bin
+    ln -s ${target} $out/bin/${name}
+'';
+in
 {
-  flameshot = pkgs.callPackage ./flameshot {};
-  knock = import ./knock;
+    ### Screenshots
+    flameshot = pkgs.callPackage ./flameshot {};
+
+    ### Ebooks
+    knock = import ./knock;
+
+    ### Text editors
+    neovim = pkgs.neovim;
+    vi = symlink (pkgs.neovim + /bin/nvim) "vi";
+    vim = symlink (pkgs.neovim + /bin/nvim) "vim";
 }
