c01b1fac0d93b9e98aee7365c73ec48e3d9b283f
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-02-11 23:58:47 -0800

    This will get handled over in `snow`.

--- bootstrap-arch.sh
+++ bootstrap-arch.sh
@@ -24,32 +24,6 @@ doit() {
     ./install
 }
 
-# TODO: port to aconfmgr.
-nuc_nas_stuff() {
-    # Enable sshd
-    enable_service sshd
-
-    # Install and configure transmission.
-    arch_package transmission-cli
-    enable_service transmission
-    # Annoyingly, if transmission seems to clobber any changes to its config
-    # files when it exists. That means we need to stop it (and wait for it to
-    # shut down) before we start tweaking its settings.
-    sudo systemctl stop transmission && sleep 1
-    sudo sed -i 's_\( *"download-dir": \).*_\1"/mnt/media/torrents",_' /var/lib/transmission/.config/transmission-daemon/settings.json
-    sudo sed -i 's_\( *"rpc-host-whitelist": \).*_\1"*",_' /var/lib/transmission/.config/transmission-daemon/settings.json
-    sudo sed -i 's_\( *"rpc-host-whitelist-enabled": \).*_\1false,_' /var/lib/transmission/.config/transmission-daemon/settings.json
-    sudo sed -i 's_\( *"rpc-whitelist": \).*_\1"*",_' /var/lib/transmission/.config/transmission-daemon/settings.json
-    sudo sed -i 's_\( *"rpc-whitelist-enabled": \).*_\1false,_' /var/lib/transmission/.config/transmission-daemon/settings.json
-    sudo systemctl start transmission
-
-    # Configure and start HTPC webui docker container.
-    docker compose -f ~/.dotfiles/containers/htpc-ui/docker-compose.yml up --detach
-
-    # Start the home-assistant docker container
-    docker compose -f ~/.dotfiles/containers/home-assistant/docker-compose.yml up --detach
-}
-
 doit
 
 echo ""
