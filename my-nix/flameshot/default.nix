83434474056b4e64357c2944c70a984cb68263af
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-02-11 20:24:50 -0800

    Oh man this has been driving me mad.
    So glad there's a patch for it =)

--- my-nix/flameshot/default.nix
+++ my-nix/flameshot/default.nix
@@ -1,5 +1,8 @@
 { pkgs }:
 
 pkgs.flameshot.overrideAttrs (oldAttrs: {
-  patches = [ ./0000-issue-1072-workaround.diff ];
+  patches = [
+    ./0000-issue-1072-workaround.diff
+    ./0001-issue-2283-workaround.diff
+  ];
 })
