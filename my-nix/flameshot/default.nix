bbfbbeabcd50dd2c48ef4844f90632af7e86c3ab
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-02-11 23:42:15 -0800

    Nixify my flameshot config as well.
    I'm tired of seeing flameshot.ini show up as modified. Now that won't
    happen anymore =)

--- my-nix/flameshot/default.nix
+++ my-nix/flameshot/default.nix
@@ -1,8 +1,25 @@
 { pkgs }:
 
-pkgs.flameshot.overrideAttrs (oldAttrs: {
+let
+patched_flameshot = pkgs.flameshot.overrideAttrs (oldAttrs: {
   patches = [
     ./0000-issue-1072-workaround.diff
     ./0001-issue-2283-workaround.diff
   ];
-})
+});
+config = ./flameshot.ini;
+config_dir = pkgs.runCommand "flameshot-config-home" {} ''
+  mkdir -p $out/flameshot
+  cp ${config} $out/flameshot/flameshot.ini
+'';
+
+in
+
+pkgs.symlinkJoin {
+  name = "flameshot";
+  paths = [ patched_flameshot ];
+  buildInputs = [ pkgs.makeWrapper ];
+  postBuild = ''
+    wrapProgram $out/bin/flameshot --set XDG_CONFIG_DIRS ${config_dir}
+  '';
+}
