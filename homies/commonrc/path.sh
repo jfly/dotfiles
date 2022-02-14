9eafa57b6e58af1a009fedb15b35261138b20769
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-02-13 20:31:31 -0800

    Simplify my `PATH` further.

--- homies/commonrc/path.sh
+++ homies/commonrc/path.sh
@@ -4,9 +4,5 @@ export PATH="/usr/bin"
 # nix-env installs binaries here.
 export PATH="$HOME/.nix-profile/bin:$PATH"
 
-# Useful binaries that I don't want to version control, probably because
-# they've got secrets in them. Hopefully I can get rid of this someday.
-export PATH="/usr/local/bin:$PATH"
-
 # Useful scripts managed in my dotfiles repo.
 export PATH="$HOME/bin:$PATH"
