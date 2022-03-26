e783e51512e07919af39a2e6187e9b92f84fe35b
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-03-25 17:40:44 -0700

    More misc PR script improvements

--- homies/bin/gen-pr-links.py
+++ homies/bin/gen-pr-links.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 
+import re
 import subprocess
+
+
+def get_joinhonor_repo_name():
+    url = subprocess.check_output(
+        ['git', 'remote', 'get-url', 'origin'], encoding='UTF-8'
+    )
+    url_re = re.compile(r"^git@github.com:joinhonor/(.*).git$")
+    m = url_re.match(url)
+    assert m is not None, f"Uhhh: {url}"
+    return m.group(1)
 
 
 def get_branchname(ref):
@@ -20,7 +31,11 @@ def generate_branch_pairs():
         branch = get_branchname(branch_ref)
         parent_branch = get_branchname(branch_ref + "^")
         pairs.append((parent_branch, branch))
-        if "origin/master" in parent_branch:
+        if (
+            "origin/HEAD" in parent_branch
+            or "origin/master" in parent_branch
+            or "origin/main" in parent_branch
+        ):
             break
 
         i += 1
@@ -34,7 +49,7 @@ def generate_ghpr_link(parent_branch, branch):
         target_repo = "joinhonor"
         parent_branch = parent_branch[len("remotes/upstream/") :]
 
-    return f"https://github.com/{target_repo}/external-api/compare/{parent_branch}...joinhonor:{branch}?expand=1"
+    return f"https://github.com/{target_repo}/{get_joinhonor_repo_name()}/compare/{parent_branch}...joinhonor:{branch}?expand=1"
 
 
 def main():
