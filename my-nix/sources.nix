fbd897972c83b613595f61fe8cabb637b58a2353
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-03-28 03:01:48 -0700

    Bumping to the latest nixpkgs-unstable.

--- my-nix/sources.nix
+++ my-nix/sources.nix
@@ -1,9 +1,9 @@
 {
   nixpkgs = import (builtins.fetchTarball {
     name = "nixpkgs-unstable";
-    url = "https://github.com/NixOS/nixpkgs/archive/639d0ff3523f1b4b0f82cf51dba4697b9c89323b.tar.gz";
+    url = "https://github.com/NixOS/nixpkgs/archive/1d08ea2bd83abef174fb43cbfb8a856b8ef2ce26.tar.gz";
     # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "1a70hh19v3x64yry97akkdmdjb6xf7h255vl4vzdm6ifaz4arb1g";
+    sha256 = "1q8p2bz7i620ilnmnnyj9hgx71rd2j6sjza0s0w1wibzr9bx0z05";
   });
   nixgl = import (builtins.fetchTarball {
     name = "nixGL";
