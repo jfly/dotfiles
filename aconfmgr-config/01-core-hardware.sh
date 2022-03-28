42663f82a89139984db4e33eb0c6360f4c397ff0
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-03-28 02:21:05 -0700

    Misc shell formatting

--- aconfmgr-config/01-core-hardware.sh
+++ aconfmgr-config/01-core-hardware.sh
@@ -1,22 +1,22 @@
 ### Disable the obnoxious PC speaker beep.
 # Trick from https://wiki.archlinux.org/title/PC_speaker#Disable_PC_Speaker
-cat > "$(CreateFile /etc/modprobe.d/nobeep.conf)" << EOF
+cat >"$(CreateFile /etc/modprobe.d/nobeep.conf)" <<EOF
 blacklist pcspkr
 EOF
 
 ### Don't suspend on closing the lid.
-cat > "$(CreateFile /etc/systemd/logind.conf.d/no-suspend-on-lidswitch.conf)" <<EOF
+cat >"$(CreateFile /etc/systemd/logind.conf.d/no-suspend-on-lidswitch.conf)" <<EOF
 [Login]
 HandleLidSwitch=ignore
 EOF
 # Configure other things to happen when the lid is closed.
-cat > "$(CreateFile /etc/acpi/events/lid)" <<EOF
+cat >"$(CreateFile /etc/acpi/events/lid)" <<EOF
 event=button/lid
 action=/home/jeremy/bin/x11-run-as jeremy lid.sh %e
 EOF
 
 ### Suspend when the user presses the power button.
-cat > "$(CreateFile /etc/systemd/logind.conf.d/suspend-on-powerbutton.conf)" <<EOF
+cat >"$(CreateFile /etc/systemd/logind.conf.d/suspend-on-powerbutton.conf)" <<EOF
 [Login]
 HandlePowerKey=suspend
 EOF
@@ -24,7 +24,7 @@ EOF
 ### Prevent unintended wakeups from suspend.
 # Without this, suspending with bluetooth devices connected would cause us to
 # immediately wake from sleep.
-cat > "$(CreateFile /etc/systemd/system/disable-bt-wakeup.service)" <<EOF
+cat >"$(CreateFile /etc/systemd/system/disable-bt-wakeup.service)" <<EOF
 # Inspired by https://bbs.archlinux.org/viewtopic.php?pid=1575617#p1575617
 # and https://www.reddit.com/r/archlinux/comments/3zxg65/how_to_permanently_change_procacpiwakeup_or/
 # and https://wiki.archlinux.org/index.php/Power_management/Suspend_and_hibernate#Instantaneous_wakeups_from_suspend.
--- homies/bin/lid.sh
+++ homies/bin/lid.sh
@@ -1,23 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-case "$1" in
-    button/lid)
-        case "$3" in
-            close)
-                if nmcli -t connection show --active | cut -d : -f 1 | grep "^\(Honor\|Honor Ethernet\|Cal 3\|Cal 3.5\|Cal\|Hen Wen\|Auto Ethernet\|Wired connection 1\)$" &> /dev/null; then
-                    logger "Ignoring lid close event because we're connected to: $(nmcli -t connection show --active | cut -d : -f 1)"
-                else
-                    slock
-                fi
-                ;;
-            open)
-                ;;
-            *)
-                logger "ACPI action undefined: $3"
-                ;;
-    esac
-    ;;
-    *)
-        logger "ACPI group/action undefined: $1 / $2"
-        ;;
-esac
+if grep -q close /proc/acpi/button/lid/*/state; then
+    if nmcli -t connection show --active | cut -d : -f 1 | grep "^\(Honor\|Honor Ethernet\|Cal 3\|Cal 3.5\|Cal\|Hen Wen\|Auto Ethernet\|Wired connection 1\)$" &>/dev/null; then
+        echo "Ignoring lid close event because we're connected to: $(nmcli -t connection show --active | cut -d : -f 1)"
+    else
+        echo "Lid is closed. Locking screen."
+        slock
+    fi
+elif grep -q open /proc/acpi/button/lid/*/state; then
+    echo "Lid is open. Doing nothing."
+fi
