25a5572d85a0a55b2c61120e0ee33d17a9c812b2
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-03-28 00:09:11 -0700

    Implement workaround for "Horrific mess when resizing terminal window"
    See https://github.com/romkatv/powerlevel10k#horrific-mess-when-resizing-terminal-window

--- homies/p10k.zsh
+++ homies/p10k.zsh
@@ -34,7 +34,10 @@
     # =========================[ Line #1 ]=========================
     # os_icon               # os identifier
     dir                     # current directory
+    time
+    command_execution_time
     vcs                     # git status
+    background_jobs
     # =========================[ Line #2 ]=========================
     newline                 # \n
     prompt_char             # prompt symbol
@@ -44,73 +47,76 @@
   # Right prompt on the last prompt line (where you are typing your commands) gets
   # automatically hidden when the input line reaches it. Right prompt above the
   # last prompt line gets hidden if it would overlap with left prompt.
-  typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
-    # =========================[ Line #1 ]=========================
-    status                  # exit code of the last command
-    command_execution_time  # duration of the last command
-    background_jobs         # presence of background jobs
-    direnv                  # direnv status (https://direnv.net/)
-    asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
-    virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
-    anaconda                # conda environment (https://conda.io/)
-    pyenv                   # python environment (https://github.com/pyenv/pyenv)
-    goenv                   # go environment (https://github.com/syndbg/goenv)
-    nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
-    nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
-    nodeenv                 # node.js environment (https://github.com/ekalinin/nodeenv)
-    # node_version          # node.js version
-    # go_version            # go version (https://golang.org)
-    # rust_version          # rustc version (https://www.rust-lang.org)
-    # dotnet_version        # .NET version (https://dotnet.microsoft.com)
-    # php_version           # php version (https://www.php.net/)
-    # laravel_version       # laravel php framework version (https://laravel.com/)
-    # java_version          # java version (https://www.java.com/)
-    # package               # name@version from package.json (https://docs.npmjs.com/files/package.json)
-    rbenv                   # ruby version from rbenv (https://github.com/rbenv/rbenv)
-    rvm                     # ruby version from rvm (https://rvm.io)
-    fvm                     # flutter version management (https://github.com/leoafarias/fvm)
-    luaenv                  # lua version from luaenv (https://github.com/cehoffman/luaenv)
-    jenv                    # java version from jenv (https://github.com/jenv/jenv)
-    plenv                   # perl version from plenv (https://github.com/tokuhirom/plenv)
-    perlbrew                # perl version from perlbrew (https://github.com/gugod/App-perlbrew)
-    phpenv                  # php version from phpenv (https://github.com/phpenv/phpenv)
-    scalaenv                # scala version from scalaenv (https://github.com/scalaenv/scalaenv)
-    haskell_stack           # haskell version from stack (https://haskellstack.org/)
-    kubecontext             # current kubernetes context (https://kubernetes.io/)
-    terraform               # terraform workspace (https://www.terraform.io)
-    # terraform_version     # terraform version (https://www.terraform.io)
-    aws                     # aws profile (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html)
-    aws_eb_env              # aws elastic beanstalk environment (https://aws.amazon.com/elasticbeanstalk/)
-    azure                   # azure account name (https://docs.microsoft.com/en-us/cli/azure)
-    gcloud                  # google cloud cli account and project (https://cloud.google.com/)
-    google_app_cred         # google application credentials (https://cloud.google.com/docs/authentication/production)
-    toolbox                 # toolbox name (https://github.com/containers/toolbox)
-    context                 # user@hostname
-    nordvpn                 # nordvpn connection status, linux only (https://nordvpn.com/)
-    ranger                  # ranger shell (https://github.com/ranger/ranger)
-    nnn                     # nnn shell (https://github.com/jarun/nnn)
-    xplr                    # xplr shell (https://github.com/sayanarijit/xplr)
-    vim_shell               # vim shell indicator (:sh)
-    midnight_commander      # midnight commander shell (https://midnight-commander.org/)
-    nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
-    # vpn_ip                # virtual private network indicator
-    # load                  # CPU load
-    # disk_usage            # disk usage
-    # ram                   # free RAM
-    # swap                  # used swap
-    todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
-    timewarrior             # timewarrior tracking status (https://timewarrior.net/)
-    taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
-    time                    # current time
-    # =========================[ Line #2 ]=========================
-    newline                 # \n
-    # ip                    # ip address and bandwidth usage for a specified network interface
-    # public_ip             # public IP address
-    # proxy                 # system-wide http/https/ftp proxy
-    # battery               # internal battery
-    # wifi                  # wifi speed
-    # example               # example user-defined segment (see prompt_example function below)
-  )
+  # NOTE: Disabled as a mitigation for "Horrific mess when resizing terminal
+  # window" https://github.com/romkatv/powerlevel10k#mitigation
+  typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=()
+  # typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
+  #   # =========================[ Line #1 ]=========================
+  #   status                  # exit code of the last command
+  #   command_execution_time  # duration of the last command
+  #   background_jobs         # presence of background jobs
+  #   direnv                  # direnv status (https://direnv.net/)
+  #   asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
+  #   virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
+  #   anaconda                # conda environment (https://conda.io/)
+  #   pyenv                   # python environment (https://github.com/pyenv/pyenv)
+  #   goenv                   # go environment (https://github.com/syndbg/goenv)
+  #   nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
+  #   nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
+  #   nodeenv                 # node.js environment (https://github.com/ekalinin/nodeenv)
+  #   # node_version          # node.js version
+  #   # go_version            # go version (https://golang.org)
+  #   # rust_version          # rustc version (https://www.rust-lang.org)
+  #   # dotnet_version        # .NET version (https://dotnet.microsoft.com)
+  #   # php_version           # php version (https://www.php.net/)
+  #   # laravel_version       # laravel php framework version (https://laravel.com/)
+  #   # java_version          # java version (https://www.java.com/)
+  #   # package               # name@version from package.json (https://docs.npmjs.com/files/package.json)
+  #   rbenv                   # ruby version from rbenv (https://github.com/rbenv/rbenv)
+  #   rvm                     # ruby version from rvm (https://rvm.io)
+  #   fvm                     # flutter version management (https://github.com/leoafarias/fvm)
+  #   luaenv                  # lua version from luaenv (https://github.com/cehoffman/luaenv)
+  #   jenv                    # java version from jenv (https://github.com/jenv/jenv)
+  #   plenv                   # perl version from plenv (https://github.com/tokuhirom/plenv)
+  #   perlbrew                # perl version from perlbrew (https://github.com/gugod/App-perlbrew)
+  #   phpenv                  # php version from phpenv (https://github.com/phpenv/phpenv)
+  #   scalaenv                # scala version from scalaenv (https://github.com/scalaenv/scalaenv)
+  #   haskell_stack           # haskell version from stack (https://haskellstack.org/)
+  #   kubecontext             # current kubernetes context (https://kubernetes.io/)
+  #   terraform               # terraform workspace (https://www.terraform.io)
+  #   # terraform_version     # terraform version (https://www.terraform.io)
+  #   aws                     # aws profile (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html)
+  #   aws_eb_env              # aws elastic beanstalk environment (https://aws.amazon.com/elasticbeanstalk/)
+  #   azure                   # azure account name (https://docs.microsoft.com/en-us/cli/azure)
+  #   gcloud                  # google cloud cli account and project (https://cloud.google.com/)
+  #   google_app_cred         # google application credentials (https://cloud.google.com/docs/authentication/production)
+  #   toolbox                 # toolbox name (https://github.com/containers/toolbox)
+  #   context                 # user@hostname
+  #   nordvpn                 # nordvpn connection status, linux only (https://nordvpn.com/)
+  #   ranger                  # ranger shell (https://github.com/ranger/ranger)
+  #   nnn                     # nnn shell (https://github.com/jarun/nnn)
+  #   xplr                    # xplr shell (https://github.com/sayanarijit/xplr)
+  #   vim_shell               # vim shell indicator (:sh)
+  #   midnight_commander      # midnight commander shell (https://midnight-commander.org/)
+  #   nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
+  #   # vpn_ip                # virtual private network indicator
+  #   # load                  # CPU load
+  #   # disk_usage            # disk usage
+  #   # ram                   # free RAM
+  #   # swap                  # used swap
+  #   todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
+  #   timewarrior             # timewarrior tracking status (https://timewarrior.net/)
+  #   taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
+  #   time                    # current time
+  #   # =========================[ Line #2 ]=========================
+  #   newline                 # \n
+  #   # ip                    # ip address and bandwidth usage for a specified network interface
+  #   # public_ip             # public IP address
+  #   # proxy                 # system-wide http/https/ftp proxy
+  #   # battery               # internal battery
+  #   # wifi                  # wifi speed
+  #   # example               # example user-defined segment (see prompt_example function below)
+  # )
 
   # Defines character set used by powerlevel10k. It's best to let `p10k configure` set it for you.
   typeset -g POWERLEVEL9K_MODE=nerdfont-complete
@@ -168,7 +174,7 @@
   # the number of prompt lines. You'll probably want to set POWERLEVEL9K_SHOW_RULER=false
   # if using this. You might also like POWERLEVEL9K_PROMPT_ADD_NEWLINE=false for more compact
   # prompt.
-  typeset -g POWERLEVEL9K_MULTILINE_FIRST_PROMPT_GAP_CHAR='─'
+  typeset -g POWERLEVEL9K_MULTILINE_FIRST_PROMPT_GAP_CHAR=' '
   if [[ $POWERLEVEL9K_MULTILINE_FIRST_PROMPT_GAP_CHAR != ' ' ]]; then
     # The color of the filler.
     typeset -g POWERLEVEL9K_MULTILINE_FIRST_PROMPT_GAP_FOREGROUND=7
